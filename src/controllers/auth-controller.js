394a9aa5d61fa82bd828bad3610e6bd31ad41b75
maiprapawin <maiprapawin@gmail.com>
2023-10-02 15:10:50 +0700

    update register response

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -41,7 +41,8 @@ exports.register = async (req, res, next) => {
       process.env.JWT_SECRET_KEY || "dddssssdfjkdfjeieiei",
       { expiresIn: process.env.JWT_EXPIRE }
     );
-    res.status(201).json({ accessToken });
+    delete user.password;
+    res.status(201).json({ accessToken, user });
   } catch (err) {
     next(err);
   }
